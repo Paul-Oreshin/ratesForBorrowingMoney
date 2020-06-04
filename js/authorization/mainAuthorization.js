ca33ac17613e1d3b063ef19d4a652c26b05cf491
Pavel Oreshin <trixterkorvo@gmail.com>
2020-06-04 18:43:58 +0300

    authorization page

---
+++ js/authorization/mainAuthorization.js
@@ -0,0 +1,8 @@
+let authorization = {
+    checkLogin: function () {
+        console.log("aaaaa")
+    },
+    transitionToNextPage: function () {
+        return location.href='mainPage.html';
+    }
+};
